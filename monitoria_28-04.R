0e86dad91d39f1aac843e8aaabd57a1b825d71e2
LaQuokka <105992734+LaQuokka@users.noreply.github.com>
2023-04-28 07:51:39 -0300

    Adição de script da monitoria de 28/04

---
+++ monitoria_28-04.R
@@ -0,0 +1,14 @@
+#monitoria 28/04/2023
+
+# Questão 1 (Wooldridge), código em resol
+
+# pacotes a serem utilizados
+  #obs.: é normal aparecer um warning de que o pacote foi construído sob outra 
+  #versão do R
+library(tidyverse)
+library(wooldridge)
+
+# --- Questão 1 ---
+# buscando dados da questão
+
+dados <- wooldridge::sleep75
--- primeiro_script.R
+++ primeiro_script.R
@@ -1,1 +1,8 @@
 #abrir script: cntrl + shift + n
+# rodar do começo até o cursor de digitação: ccntrl + alt + p
+# rodar uma linha na qual o cursor está: cntrl +  enter
+
+5 + 3
+4 + 8
+
+getwd() # printa no console o diretório em que o script está armazenado
