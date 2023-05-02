925be5f2fe91ca92ccb8072234c67b57ae0f8dd0
LaQuokka <105992734+LaQuokka@users.noreply.github.com>
2023-05-02 16:37:57 -0300

    Carregando dados FRI e RP
    Carregando dados produzidos via Python do FRI (resultados preliminares) e dados do resultado primário da união de 2000 a 2021 para o R

--- learning_correlation.R
+++ learning_correlation.R
@@ -3,7 +3,18 @@
   #Objetivo: rodar uma correlação entre resultado primário da União e
   #índice de força do arcabouço fiscal para cada ano no brasil de 2000-2021
 
+#fonte: http://sthda.com/english/wiki/correlation-test-between-two-variables-in-r
+
 library("ggpubr")
 my_data <- mtcars
 head(my_data, 6)
 my_data
+#descriptions(my_data)
+ggscatter(my_data, x = "mpg", y = "wt", 
+          xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+
+# tentando importar meus próprios dados
+library('readxl')
+dados_fri = read_excel('G:\\Meu Drive\\Projetos Atuais\\IC\\VS\\data_for_corr.xlsx')
+dados_fri
+ggscatter(dados_fri, x = 'resultado_primario', y = 'FRI')
