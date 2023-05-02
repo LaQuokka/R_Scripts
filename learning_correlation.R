a2f9e9b44502030dc6013539c301810bdda48510
LaQuokka <105992734+LaQuokka@users.noreply.github.com>
2023-05-02 15:42:25 -0300

    subindo arquivo com estudo de correlação

---
+++ learning_correlation.R
@@ -0,0 +1,9 @@
+  
+# APRENDENDO A RODAR CORRELAÇÕES
+  #Objetivo: rodar uma correlação entre resultado primário da União e
+  #índice de força do arcabouço fiscal para cada ano no brasil de 2000-2021
+
+library("ggpubr")
+my_data <- mtcars
+head(my_data, 6)
+my_data
